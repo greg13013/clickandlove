03edf4e3888cfc9588a62529147968bd46689c98
Colli Gregory <colli.gregory.13@gmail.com>
2021-06-08 11:51:30 +0200

    ajout css image background et overlay

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import firebase from 'firebase';
+import {AuthService} from "./services/auth.service";
 
 @Component({
   selector: 'app-root',
@@ -10,7 +11,9 @@ export class AppComponent {
   title = 'clickandlove';
   db: any;
 
-  constructor() {
+  constructor(private authService: AuthService) {
+
+    console.log(authService.isAuth)
     const config = {
       apiKey: "AIzaSyBoABOi2zE-GS63KeuRYt42uyAHzvicJz0",
       authDomain: "clickandlove-8e069.firebaseapp.com",
@@ -21,5 +24,8 @@ export class AppComponent {
     };
     let app = firebase.initializeApp(config);
    this.db = firebase.firestore(app);
+
+   //Initialise la déconnexion lors du rafraichissement
+    authService.signOutUser();
   }
 }
